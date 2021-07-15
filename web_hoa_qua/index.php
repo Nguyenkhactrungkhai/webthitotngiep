d5e10cdc4516d69dd06bdbff2a7ed05581560a03
Nguyen Khac Trung Khai <nguyenkhactrungkhai@gmail.com>
2021-07-15 09:46:02 +0700

    web hoa qua

---
+++ web_hoa_qua/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+	<div class="container"> 
+    	<div class="menu"> <img src="images/nhom 2.png" width="62" height="35" />
+        <input type="text" />
+        <ul> 
+        	<li> <a href="#">TRANG CHỦ </a></li>
+            <li> <a href="#">GIỚI THIỆU</a></li>
+            <li> <a href="#">LIÊN HỆ</a></li>
+            <li> <a href="#">GIỚI THIỆU </a></li>
+        </ul>
+        </div>
+        <div class="content">
+        	<div class="left"> 
+            	<p>  DANH MỤC </p>
+            	<ul> 
+                	<li><a href="#"> Trai cay My</a></li>
+                    <li><a href="#">  Trai cay Uc </a> </li>
+                   	<li><a href="#"> Trai cay New Zealand </a> </li>
+                    <li><a href="#">  Trai cay Trung Quoc</a></li>
+                    <li><a href="#"> Trai cay Han Quoc</a></li>
+                    <li><a href="#"> Trai cay Chi le</a> </li>
+                    <li><a href="#">  Trai cay Nam Phi</a></li>
+                    <li><a href="#"> Trai cay Canada</a> </li>
+                </ul>
+            	<img style="margin:11px 0;" src="images/image_07.png" width="120" height="415" />
+            </div>
+            <div class="right"><img src="images/index_main_bnr.png" width="337" height="150" /> 
+                  <img style="margin-top:20px;" src="images/image_06.png" width="337" height="40" />
+                        			  <table border="1">
+                  	<tr> 
+                    	<td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                         <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                        <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                    </tr>
+                                      	<tr> 
+                    	<td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                         <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                        <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                    </tr>
+                                      	<tr> 
+                    	<td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                         <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                        <td> <img src="images/product-02.png" width="106" height="83" />
+                        	<p class="pTim"> Quýt Úc</p>
+                           	<p class="pCam"> 150.000 đ </p>
+                        </td>
+                    </tr>
+                  </table>
+             </div>
+			<div class="clr"> </div>
+        </div>
+
+        <div class="footer"> </div>
+    </div>
+</body>
+</html>
